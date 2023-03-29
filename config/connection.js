ea8ff0405af8c0f32e7c7f4b3cc1ab98f0b7e761
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-03-28 22:00:30 -0500

    finished setting up the models

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
+// jawsdb is the way we're connecting mySQL to Heroku
+// checks if you have a jawsdb connection, if not, it uses the local
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "localhost",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
